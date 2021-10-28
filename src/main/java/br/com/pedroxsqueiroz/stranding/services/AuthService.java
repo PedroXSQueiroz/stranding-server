adb9bed4c03da75f2d96d597e11c91deb0115338
pedro.queiroz <pedro.queiroz@mindsatwork.com.br>
2021-10-27 21:58:29 -0300

    Removing unused classes

--- src/main/java/br/com/pedroxsqueiroz/stranding/services/AuthService.java
+++
@@ -1,5 +0,0 @@
-package br.com.pedroxsqueiroz.stranding.services;
-
-public interface AuthService {
-
-}
--- src/main/java/br/com/pedroxsqueiroz/stranding/services/impl/AuthServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package br.com.pedroxsqueiroz.stranding.services.impl;
-
-import br.com.pedroxsqueiroz.stranding.services.AuthService;
-
-public class AuthServiceImpl implements AuthService {
-
-}
